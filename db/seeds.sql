077689243fa6301f5a80f854a51780c92861627b
Jadelet <slaethe@gmail.com>
2018-10-22 19:02:09 -0400

    initial setup

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Bleu Cheese');
+INSERT INTO burgers (name) VALUES ('Bacon Avocado');
+INSERT INTO burgers (name, sleepy) VALUES ('SPicy Jalapeno baja', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Truffle Mushroom Swiss', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Joe Biden', true);
+INSERT INTO burgers (name) VALUES ('Owens');
