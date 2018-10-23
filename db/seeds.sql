3f03814a0365941804380367eaa9dae268b69b77
Jadelet <slaethe@gmail.com>
2018-10-22 21:59:20 -0400

    Burgerized

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,5 @@
 INSERT INTO burgers (name) VALUES ('Bleu Cheese');
 INSERT INTO burgers (name) VALUES ('Bacon Avocado');
-INSERT INTO burgers (name, sleepy) VALUES ('SPicy Jalapeno baja', true);
-INSERT INTO burgers (name, sleepy) VALUES ('Truffle Mushroom Swiss', true);
-INSERT INTO burgers (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO burgers (name) VALUES ('Owens');
+INSERT INTO burgers (name, devoured) VALUES ('SPicy Jalapeno baja', true);
+INSERT INTO burgers (name, devoured) VALUES ('Truffle Mushroom Swiss', true);
+
